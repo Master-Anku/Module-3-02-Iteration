59d1a5d7f6822c2e0e76c5ef1f951c71768a70e8
Ankura <Master-Anku.com>
2024-05-01 06:36:19 +0630

    Iteration

---
+++ script.js
@@ -0,0 +1,6 @@
+// TODO: Write Your JavaScript Code Here
+var students = ['Ko ', 'Hnin', 'Maung', 'Anku', 'Ankura'];
+//console.log(students.length);
+
+for (var i = 0; i <students.length; i++){
+    console.log('Great to see you, ' + students[i] + ' CLASSMATE_NAME! ');}
